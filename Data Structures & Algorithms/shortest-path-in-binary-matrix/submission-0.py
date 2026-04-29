2a9f203f88e300adc1da347ae274056f782c9375
Jeff Shin <yungjae.jeffshin@gmail.com>
2026-04-28 21:02:04 -0400

    Add: shortest-path-in-binary-matrix - submission-0

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-0.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        if grid[0][0] or grid[ROWS - 1][COLS - 1]:
+            return -1
+
+        queue = deque()
+        visited = set((0,0))
+        dirs = [(-1 , 0), (0, -1), (1, 0), (0, 1), (-1, -1), (1,1), (-1, 1), (1, -1)]
+
+        queue.append((0,0,1))
+
+        while queue:
+            for i in range(len(queue)):
+                r,c, length = queue.popleft()
+                if r == ROWS - 1 and c == COLS - 1:
+                    return length
+                
+                for dr, dc in dirs:
+                    row, col = r + dr, c + dc
+                    if min(row, col) < 0 or row == ROWS or col == COLS or (row, col) in visited or grid[row][col] == 1:
+                        continue
+                    queue.append((row, col, length + 1))
+                    visited.add((row, col))
+        
+        return -1 
