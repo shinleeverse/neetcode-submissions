0f20582ffc7c4f4539f81985bdd631384e39ec85
Jeff Shin <yungjae.jeffshin@gmail.com>
2026-05-03 13:46:36 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = defaultdict(int)
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in visited:
+                return [visited[diff], i]
+            visited[nums[i]] = i
+        return [-1, -1]
