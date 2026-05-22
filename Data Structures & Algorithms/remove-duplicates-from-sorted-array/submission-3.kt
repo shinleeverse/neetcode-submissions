308a66d8d2008a4ff982021efb8de827f6c51b62
Jeff Shin <yungjae.jeffshin@gmail.com>
2026-05-22 15:04:33 -0400

    Add: remove-duplicates-from-sorted-array - submission-3

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-3.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        if(nums.isEmpty()){
+            return 0
+        }
+        
+        var k = 1
+        for(i in 1 until nums.size){
+            if(nums[i] != nums[i - 1]){
+                nums[k++] = nums[i]
+            }
+        }
+
+        return k
+    }
+}
