2af7f8cbba9a6aeb5f015e1fdc250bf83c8f1719
Jeff Shin <yungjae.jeffshin@gmail.com>
2026-05-21 21:32:13 -0400

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val sb = StringBuilder()
+        for(pointer in 0 until maxOf(word1.length, word2.length)){
+            if(pointer < word1.length){
+                sb.append(word1[pointer])
+            }
+            if(pointer < word2.length){
+                sb.append(word2[pointer])
+            }
+        }
+        return sb.toString()
+    }
+}
