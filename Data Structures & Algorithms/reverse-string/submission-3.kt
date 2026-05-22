b2c5351bea18be854616a5332a8acbe4b663cdcc
Jeff Shin <yungjae.jeffshin@gmail.com>
2026-05-22 14:17:59 -0400

    Add: reverse-string - submission-3

---
+++ Data Structures & Algorithms/reverse-string/submission-3.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun reverseString(s: CharArray) {
+        var left = 0
+        var right = s.size - 1
+
+        while(left <= right){
+            val temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left++
+            right--
+        }
+    }
+}
